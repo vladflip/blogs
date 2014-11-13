45d492d46167c7092fbaae755ed0ad03f2cd2897
flip <sef@sdf.fds>
2014-11-13 14:41:28 +0200

    sd

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -32,10 +32,11 @@ $app = new Illuminate\Foundation\Application;
 // ));
 // 
 $env = $app->detectEnvironment(function(){
-	if(gethostname() == "hugo.beget.ru") return "production";
-    if($_SERVER['HTTP_HOST'] == "hugo.beget.ru") return "production";
-    if(getenv("APP_ENV")!="") return getenv("APP_ENV");
-    return "local";
+	// if(gethostname() == "hugo.beget.ru") return "production";
+ //    if($_SERVER['HTTP_HOST'] == "hugo.beget.ru") return "production";
+ //    if(getenv("APP_ENV")!="") return getenv("APP_ENV");
+ //    return "local";
+ 		return 'production';
 });
 
 /*
