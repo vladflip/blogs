f0366d33c1aa07147602192c9d1393fcc68a8d77
flip <sef@sdf.fds>
2014-11-13 14:29:28 +0200

    db

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'production' => array('блоги.патриотки.рф'),
+	'production' => array('blogs.patriotki.ru'),
 	'local' 	 => array('FLIP-Thinkpad')
 
 ));
