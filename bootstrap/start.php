20dac1246ca93d8b60bf53a84604137734ba916e
flip <sef@sdf.fds>
2014-11-13 03:25:44 +0200

    config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'production' => array('hugo.beget.ru'),
+	'local' 	 => array('FLIP-Thinkpad')
 
 ));
 
