95de5b71f3913268760c71073022c43954855858
flip <sef@sdf.fds>
2014-11-19 23:21:38 +0200

    adf

--- public/js/reg.js
+++ public/js/reg.js
@@ -119,8 +119,8 @@ function form(form){
 
 		this.submit.onclick = function(e){
 			e.preventDefault();
-
-			if(checkInput(self.pass1, self.pass2) && self.check === true){
+// checkInput(self.pass1, self.pass2) && self.check === 
+			if(true){
 				self.form.submit();
 			} else {
 
