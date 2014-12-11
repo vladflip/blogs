5f8a3b7295a0a4ee5b7990b46febec97f387dfcb
flip <sef@sdf.fds>
2014-12-12 00:27:10 +0200

    updated guest profile like owners profile

--- app/views/guest_profile.blade.php
+++ app/views/guest_profile.blade.php
@@ -1,34 +1,98 @@
-@extends('layouts.profile')
+@extends('layouts.main')
 
-@section('ava_xl')
-	@if(empty($user->ava_xl))
-		<img id="avaEl" src="{{ 'img/q_mark.png' }}" alt="">
-	@else
-		<img id="avaEl" src="{{ $user->ava_xl }}" alt="">
-	@endif
+@section('head')
+	@parent
+	<meta name="_token" content="{{ csrf_token() }}" />
+	<link rel="stylesheet" href="css/jquery.Jcrop.min.css">
 @stop
 
-@section('login')
-	<span>{{ $user->login or 'логин' }}</span>
-@stop
+@section('body')
+	<div class="profile">
 
-@section('fname')
-	<span>{{ $user->firstname or 'имя' }}</span>
-@stop
+		<div class="p_user-ava-block">
 
-@section('lname')
-	<span>{{ $user->lastname or 'фамилия' }}</span>
-@stop
+			<div class="p_avatar" id="pAvatar">
 
-@section('age')
-	<span>{{ $user->age or '0' }}</span>
-@stop
+				@if(!empty($user->ava_xl)&&file_exists($user->ava_xl))
+					<img id="avaEl" src="{{ $user->ava_xl }}" alt="">
+				@else
+					<img id="avaEl" src="{{ 'img/q_mark.png' }}" alt="">	
+				@endif
 
-@section('town')
-	<span>{{ $user->town or 'город' }}</span>
-@stop
+			</div>
 
-@section('about')
-	<textarea name="" id="" disabled cols="40" rows="5">{{ $user->about or 'о себе' }}</textarea>
+			<div class="p_rate-block">
+				<div class="p_rate-label">Рейтинг активности</div>
+				<div class="p_rate data-in" id="pRate">0</div>
+			</div>
+
+		</div>
+
+		<div class="p_user-info">
+
+			<ul class="p_info-list">
+
+				<li>
+					<div class="p_login" id="pLogin">
+
+						<span>
+							{{{ $user->login or 'логин' }}}
+						</span>
+
+					</div>
+				</li>
+				
+				<li>
+					<div class="p_name-block">
+						<div class="p_name data-in" id="pName">
+
+							<span>
+								{{ $user->name or 'имя' }}
+							</span>
+
+						</div>
+					</div>
+							
+					<div class="p_age-block">
+						<div class="p_age data-in" id="pAge">
+							
+							<span>
+								{{ $user->age or '0' }}
+							</span>
+
+						</div>
+					</div>
+				</li>
+				
+				<li>
+					<div class="p_town-block">
+						<div class="p_town data-in" id="pTown">
+
+							<span>
+								{{ $user->town or 'город' }}
+							</span>
+
+						</div>
+					</div>
+				</li>
+				
+				<li>
+					<div class="p_about-block">
+						
+						@if($user->about)
+							<div class="p_about-cap ready">
+								{{ $user->about}}
+							</div>
+						@else
+							<div class="p_about-cap">
+								{{ 'Нет инфо о пользователе' }}
+							</div>
+						@endif
+
+					</div>
+				</li>
+			</ul>
+
+		</div>
+	</div>
 @stop
-asdflkjasdfkljasdf
