ea5daa3d0ca2501620b09455de774ce690b5be35
flip <sef@sdf.fds>
2015-02-01 16:47:23 +0200

    mail test 3

--- app/views/emails/verify.blade.php
+++ app/views/emails/verify.blade.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	Докажите что вы не робот :)
-	<a href="{{ home_url() . /register/verify/ . $code }}">
+	<a href="{{ URL::to('/') . '/register/verify/' . $code }}">
 		Нажмите сюда
 	</a>
 </body>
