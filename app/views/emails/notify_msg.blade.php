e6e332600ad2b36006aa3cf5da6804e9a708feff
flip <sef@sdf.fds>
2015-02-03 14:43:06 +0200

    msg

--- app/views/emails/notify_msg.blade.php
+++ app/views/emails/notify_msg.blade.php
@@ -1,3 +1,3 @@
 (<a href="блоги.патротки.рф">блоги.патриотки.рф</a>) <br>
 Сообщение от <a href="{{ route('profile_id', $from->id) }}">{{ $from->name }}</a>. <br>
-Перейти к сообщениям {{ route('messages') }}.
+Перейти к <a href="{{ route('messages') }}">сообщениям</a>.
